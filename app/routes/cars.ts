51c3b17b0dfb678aeca462520fa1c5513b6c54c5
Jean Danilo Maxwell Patrick Pootaren <danilo.pootaren@hangarww.com>
2021-09-19 20:20:21 +0400

    Added layout

---
+++ app/routes/cars.ts
@@ -0,0 +1,12 @@
+import Route from '@ember/routing/route';
+
+
+export default class Cars extends Route.extend({
+  // anything which *must* be merged to prototype here
+}) {
+  async model(){
+    const response = await fetch('/MOCK_VEHICLE_DATA.json')
+    const cars = await response.json()
+    return {cars}
+  }
+}
